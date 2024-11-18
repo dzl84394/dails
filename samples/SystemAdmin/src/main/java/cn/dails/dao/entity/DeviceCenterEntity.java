56908b12e4a930f98d6c22ff679758f5c4e0ea68
dzl84394 <dzl84394@qq.com>
2024-11-18 22:39:49 +0800

    jdk8 配置管理

---
+++ samples/SystemAdmin/src/main/java/cn/dails/dao/entity/DeviceCenterEntity.java
@@ -0,0 +1,45 @@
+package cn.dails.dao.entity;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.Data;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "dails_sub_devicecenter")
+@TableName(value = "dails_sub_device",autoResultMap = true)
+@Data
+public class DeviceCenterEntity extends BaseEntity implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @TableId(type = IdType.AUTO)
+    private Long id;
+    private String deviceType;//数据中心，机房，机柜
+    //机房
+    // ，机柜，机位，物理机，存储，网络设备，云主机，pks
+    private String deviceCenter;
+    private String deviceCenterSn;
+    //机房
+    private String deviceRoom;//三号楼4层，a机房
+    private String deviceRoomSn;
+
+    //机柜
+    private String cabinetName;
+    private String rowcell;//位置，搂层房间号-X排Y行-第几个
+    private String cabinetSn;
+
+    //物理机
+    private String rack;//机位，45U
+    private int high = 1;//3U
+    private String deviceSn;//SZF-1121A-1205-3U
+
+
+
+
+    private String env;//DMZ,SF,STG,网管区，环境
+    private String status;//Active', 'Inactive', 'Maintenance'
+}
--- samples/SystemAdmin/src/main/java/cn/dails/dao/entity/DeviceEntity.java
+++ samples/SystemAdmin/src/main/java/cn/dails/dao/entity/DeviceEntity.java
@@ -18,7 +18,7 @@ public class DeviceEntity extends BaseEntity implements Serializable {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @TableId(type = IdType.AUTO)
     private Long id;
-    private String deviceType;//
+    private String deviceType;// 服务器、network网络设备和存储设备
     //机房
     // ，机柜，机位，物理机，存储，网络设备，云主机，pks
     private String deviceCenter;
@@ -28,20 +28,23 @@ public class DeviceEntity extends BaseEntity implements Serializable {
     private String deviceRoomSn;
 
     //机柜
+    private String cabinetName;
     private String rowcell;//位置，搂层房间号-X排Y行-第几个
-    private String rowcellSn;
+    private String cabinetSn;
 
-    //物理机
-    private String seat;//机位，45U
-    private String seatNum;//3U
-    private String seatSn;//SZF-1121A-1205-3U
+    private String manufacturern;//制造商，表示物理机的制造商信息。
+    private String modeln;;//型号，表示物理机的型号信息。
+
+    private String cpuModeln;//CPU 型号，表示物理机所搭载的 CPU 的型号。
+    private String  cpuCores;//CPU 核心数，表示物理机 CPU 的核心数量。
+    private String memorySize;//内存大小，表示物理机的内存容量。
+    private String storageSize;//存储容量，表示物理机的存储容量。
+    private String networkInterfaces;//网络接口，表示物理机的网络接口信息。
+    private String powerSupply;//电源信息，表示物理机的电源供应情况。
+    private String os;//操作系统，表示物理机所安装的操作系统信息。
 
 
-    private String lscpu;//cpu信息
-    private String freem;//内存情况
-    private String disk;//磁盘情况
-    private String os;
-    private String ip;//依赖设备.网络设备忽略
+
 
     private String env;//DMZ,SF,STG,网管区，环境
     private String status;//Active', 'Inactive', 'Maintenance'
